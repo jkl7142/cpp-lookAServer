54bb0ee32dc99e0e41941c0c8038c408da9f1d3d
jkl7142 <jkl7142@naver.com>
2021-09-30 03:03:48 -0700

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -7,14 +7,24 @@
 
 #include "targetver.h"
 
-#define _WIN32_WINNT 0x0600
-
-#include <stdio.h>
+#ifdef _WIN32
+#include <Ws2tcpip.h>
+#include <winsock2.h>
 #include <tchar.h>
-#include <windows.h>
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
+
+#include <assert.h>
+#include <exception>
 #include <iostream>
-#include <thread>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <cstdint>
+
+#include <vector>
 #include <string>
 #include <list>
 #include <map>
-#include <mutex>
